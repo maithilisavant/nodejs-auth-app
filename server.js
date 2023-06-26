b0f1a1f3218cb5bbd7b5da6b30c860c7e547ffe8
maithilisavant <maithili.savant@mindbowser.com>
2023-06-26 23:07:28 +0530

    updated server.js

--- server.js
+++ server.js
@@ -22,24 +22,6 @@ const server = app.listen(8080, () => {
   console.log("Server listening on port 8080");
 });
 
-const io = require("socket.io")(server, {
-  allowEIO3: true,
-  cors: {
-    origin: true,
-    methods: ['GET', 'POST'],
-    credentials: true
-  }
-});
-
 const jwt = require("jwt-then");
 
 const User = mongoose.model("User");
-
-io.use(async (socket, next) => {
-  try {
-    const token = socket.handshake.query.token;
-    const data = await jwt.verify(token, process.env.SECRET);
-    socket.userId = data.id;
-    next();
-  } catch (err) {}
-});
